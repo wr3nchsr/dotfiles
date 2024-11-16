f30ccc367f54e26bf28e77b2fe19af3d8b05d73f
wr3nchsr <45386823+wr3nchsr@users.noreply.github.com>
2024-11-16 13:50:03 +0300

    update setup script

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ SCRIPT_PATH=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
 # tmux configurations and plugins
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s ${SCRIPT_PATH}/configs/tmux.conf ~/.tmux.conf
-
+ln -s ${SCRIPT_PATH}/configs/tmux/tmux.conf ~/.tmux.conf
+ln -s ${SCRIPT_PATH}/configs/tmux/custom-status ~/.tmux/plugins/custom-status
 mkdir -p ~/.config
 
 # alacritty configurations and theme
